7ec83fbb3ba566561f5c9d086ed9d2d4192e978e
Rishith Kamishetty <kamishetty.rishith@gmail.com>
2024-11-11 14:00:03 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,14 +4,11 @@ def decode_message( s: str, p: str) -> bool:
         m = len(s)
         n = len(p)
         dp = [[False] * (m + 1) for _ in range(n + 1)]
-        dp[0][0] = True  # Empty pattern matches empty message
+        dp[0][0] = True 
 
-        # Handle patterns that start with '*'
         for i in range(1, n + 1):
                 if p[i - 1] == '*':
                         dp[i][0] = dp[i - 1][0]
-        
-        # Fill the DP table
         for i in range(1, n + 1):
                 for j in range(1, m + 1):
                         if p[i - 1] == s[j - 1] or p[i - 1] == '?':
