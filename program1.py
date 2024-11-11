2754076f2c04005df2da3f9dba007b06ac9af81d
Rishith Kamishetty <kamishetty.rishith@gmail.com>
2024-11-11 13:57:04 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,8 +2,7 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here  
-
-            if not grid:
+        if not grid:
             return 0
         
         def dfs(x, y):
