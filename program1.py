e07522c47473973886193e9698221ea95de5ca3e
Rishith Kamishetty <kamishetty.rishith@gmail.com>
2024-11-11 13:56:43 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,31 +4,31 @@ class Solution:
     #    write your code here  
 
             if not grid:
-        return 0
-    
-    def dfs(x, y):
-        # Check boundaries and ensure the cell is land
-        if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]) or grid[x][y] == 'W':
-            return
-        # Mark the current cell as visited by setting it to 'W'
-        grid[x][y] = 'W'
+            return 0
         
-        # Explore all four directions
-        dfs(x + 1, y)  # down
-        dfs(x - 1, y)  # up
-        dfs(x, y + 1)  # right
-        dfs(x, y - 1)  # left
-    
-    island_count = 0
-    
-    # Traverse every cell in the grid
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            # If cell is land, it's a new island
-            if grid[i][j] == 'L':
-                dfs(i, j)
-                island_count += 1
-    
-    return island_count
-                    
-        return 0
+        def dfs(x, y):
+            # Check boundaries and ensure the cell is land
+            if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]) or grid[x][y] == 'W':
+                return
+            # Mark the current cell as visited by setting it to 'W'
+            grid[x][y] = 'W'
+            
+            # Explore all four directions
+            dfs(x + 1, y)  # down
+            dfs(x - 1, y)  # up
+            dfs(x, y + 1)  # right
+            dfs(x, y - 1)  # left
+        
+        island_count = 0
+        
+        # Traverse every cell in the grid
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                # If cell is land, it's a new island
+                if grid[i][j] == 'L':
+                    dfs(i, j)
+                    island_count += 1
+        
+        return island_count
+                        
+            return 0
