70c2d8f7c5fc464318d46125a47344e4f87f07a9
Rishith Kamishetty <kamishetty.rishith@gmail.com>
2024-11-11 14:00:22 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,24 +6,18 @@ class Solution:
             return 0
         
         def dfs(x, y):
-            # Check boundaries and ensure the cell is land
             if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]) or grid[x][y] == 'W':
                 return
-            # Mark the current cell as visited by setting it to 'W'
             grid[x][y] = 'W'
             
-            # Explore all four directions
-            dfs(x + 1, y)  # down
-            dfs(x - 1, y)  # up
-            dfs(x, y + 1)  # right
-            dfs(x, y - 1)  # left
+            dfs(x + 1, y)  
+            dfs(x - 1, y)  
+            dfs(x, y + 1)  
+            dfs(x, y - 1) 
         
         island_count = 0
-        
-        # Traverse every cell in the grid
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                # If cell is land, it's a new island
                 if grid[i][j] == 'L':
                     dfs(i, j)
                     island_count += 1
