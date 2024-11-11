b5ec738271f2a0c3bfca54d78e19e791848239c5
Rishith Kamishetty <kamishetty.rishith@gmail.com>
2024-11-11 13:59:40 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -18,7 +18,7 @@ def decode_message( s: str, p: str) -> bool:
                 for j in range(1, m + 1):
                         if p[i - 1] == s[j - 1] or p[i - 1] == '?':
                                 dp[i][j] = dp[i - 1][j - 1]
-                        elif s[i - 1] == '*':
+                        elif p[i - 1] == '*':
                                 dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
         
         return dp[n][m]
